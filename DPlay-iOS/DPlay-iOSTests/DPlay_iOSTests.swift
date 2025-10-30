12f05e7f6661d7633f485415a1aefadb6d38bd01
정정욱 <jeonguk29@naver.com>
2025-10-30 14:07:04 +0900

    [Build] 프로젝트 폴더링 작업 및 환경 설정

---
+++ DPlay-iOS/DPlay-iOSTests/DPlay_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  DPlay_iOSTests.swift
+//  DPlay-iOSTests
+//
+//  Created by 정정욱 on 10/30/25.
+//
+
+import Testing
+@testable import DPlay_iOS
+
+struct DPlay_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
