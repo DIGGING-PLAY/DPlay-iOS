80f03929c5aff67b151adec33305f587124dfd61
정정욱 <jeonguk29@naver.com>
2025-11-07 17:12:59 +0900

    [Feat] #7 - Question, Recommendation Entity 생성

---
+++ DPlay-iOS/DPlay-iOS/Domain/Home/Entity/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  DPlay-iOS
+//
+//  Created by 정정욱 on 11/7/25.
+//
+
+import Foundation
+
+struct Question {
+    let id: Int
+    let date: String
+    let content: String
+}
---
+++ DPlay-iOS/DPlay-iOS/Domain/Home/Entity/Recommendation.swift
@@ -0,0 +1,18 @@
+//
+//  Recommendation.swift
+//  DPlay-iOS
+//
+//  Created by 정정욱 on 11/7/25.
+//
+
+import Foundation
+
+struct Recommendation {
+    let id: Int
+    let userName: String
+    let songTitle: String
+    let artist: String
+    let comment: String
+    let likeCount: Int
+    let isScrapped: Bool
+}
