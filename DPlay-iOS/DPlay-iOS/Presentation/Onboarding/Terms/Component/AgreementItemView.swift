43c77f42e40bbcaaba1f88d2cb962dee96c36b86
JoHyerin <gpfls468@gmail.com>
2025-11-17 06:02:35 +0900

    [Feat] #11 - 약관 component 구현 (AllAgreementButton, AgreementItemView)

---
+++ DPlay-iOS/DPlay-iOS/Presentation/Onboarding/Terms/Component/AgreementItemView.swift
@@ -0,0 +1,85 @@
+//
+//  AgreementItemView.swift
+//  DPlay-iOS
+//
+//  Created by 조혜린 on 11/15/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class AgreementItemView: UIView {
+        
+    //MARK: - UI Properties
+
+    let agreeButton = UIButton()
+    private let titleLabel = UILabel()
+    private let arrowImageView = UIImageView(image: UIImage(resource: .icArrowRight16))
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension AgreementItemView {
+    
+    //MARK: - setup
+    
+    func setupStyle() {
+        agreeButton.do {
+            $0.setImage(.icCheckCircleDefault24, for: .normal)
+            $0.setImage(.icCheckCircleSelected24, for: .selected)
+        }
+        
+        titleLabel.do {
+            $0.setTextStyle(.bodySemi16)
+            $0.textColor = .gray500
+        }
+    }
+    
+    func setupHierarchy() {
+        addSubviews(
+            agreeButton,
+            titleLabel,
+            arrowImageView
+        )
+    }
+    
+    func setupLayout() {
+        agreeButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(agreeButton.snp.trailing).offset(12)
+        }
+
+        arrowImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.trailing.equalToSuperview().inset(12)
+        }
+    }
+}
+
+extension AgreementItemView {
+    
+    //MARK: - Method
+    
+    func setTitle(_ text: String) {
+        titleLabel.text = text
+    }
+}
---
+++ DPlay-iOS/DPlay-iOS/Presentation/Onboarding/Terms/Component/AllAgreementButton.swift
@@ -0,0 +1,65 @@
+//
+//  AllAgreementButton.swift
+//  DPlay-iOS
+//
+//  Created by 조혜린 on 11/15/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class AllAgreementButton: UIButton {
+        
+    //MARK: - UI Properties
+
+    private var checkImageView = UIImageView(image: UIImage(resource: .icCheckCircleDefault24))
+    private let customTitleLabel = UILabel()
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupStyle()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension AllAgreementButton {
+    
+    //MARK: - setup
+    
+    func setupStyle() {
+        backgroundColor = .gray100
+        roundCorners(cornerRadius: 12)
+        
+        customTitleLabel.do {
+            $0.text = "네, 모두 동의합니다"
+            $0.setTextStyle(.bodySemi16)
+            $0.textColor = .dplay_black
+        }
+    }
+    
+    func setupHierarchy() {
+        addSubviews(checkImageView, customTitleLabel)
+    }
+    
+    func setupLayout() {
+        checkImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalToSuperview().inset(12)
+        }
+        
+        customTitleLabel.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(checkImageView.snp.trailing).offset(12)
+        }
+    }
+}
