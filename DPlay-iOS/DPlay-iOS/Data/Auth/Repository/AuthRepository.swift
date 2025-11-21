3b89a9cd564fd0f15644c3d64fec55d2fb1ce827
JoHyerin <gpfls468@gmail.com>
2025-11-22 03:06:53 +0900

    [Add] #8 - AuthService 추가

--- DPlay-iOS/DPlay-iOS/Data/Auth/Repository/AuthRepository.swift
+++ DPlay-iOS/DPlay-iOS/Data/Auth/Repository/AuthRepository.swift
@@ -7,20 +7,33 @@
 
 protocol AuthRepository {
     func loginWithApple(appleIdentityToken: String) async throws -> UserSession
-    func refreshAccessToken(refreshToken: String) async throws -> AuthToken
+    func refreshAccessToken(refreshToken: String) async throws -> UserSession
     func logout() async throws
 }
 
 final class DefaultAuthRepository: AuthRepository {
+    
+    private let service: AuthService
+    
+    init(service: AuthService) {
+        self.service = service
+    }
+
     func loginWithApple(appleIdentityToken: String) async throws -> UserSession {
-        return UserSession(userId: 0, userTokens: AuthToken(accessToken: "", refreshToken: ""))
+        let response = try await service.loginWithApple(appleIdentityToken: appleIdentityToken)
+        let entity = response.toEntity()
+
+        return entity
     }
     
-    func refreshAccessToken(refreshToken: String) async throws -> AuthToken {
-        return AuthToken(accessToken: "", refreshToken: "")
+    func refreshAccessToken(refreshToken: String) async throws -> UserSession {
+        let response = try await service.refreshAccessToken(refreshToken: refreshToken)
+        let entity = response.toEntity()
+
+        return entity
     }
     
-    func logout() {
-        print("로그아웃")
+    func logout() async throws {
+        try await service.logout()
     }
 }
