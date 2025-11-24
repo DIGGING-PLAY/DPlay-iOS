0ecce02cf309442910157efd7846f93cbc2b48e9
JoHyerin <gpfls468@gmail.com>
2025-11-24 20:23:33 +0900

    [Fix] #11 - 회원가입 관련 로직 Onboarding -> Auth로 이동

--- DPlay-iOS/DPlay-iOS/Data/Onboarding/Repository/OnboardingRepository.swift
+++ DPlay-iOS/DPlay-iOS/Data/Onboarding/Repository/OnboardingRepository.swift
@@ -8,16 +8,9 @@
 import Foundation
 
 protocol OnboardingRepository {
-    func validateNicknameDuplicate(nickname: String) async throws -> Bool
-    func singUp(nickname: String, image: Data) async throws
+    
 }
 
 final class DefaultOnboardingRepository: OnboardingRepository {
-    func validateNicknameDuplicate(nickname: String) async throws -> Bool {
-        return true
-    }
-        
-    func singUp(nickname: String, image: Data) async throws {
-        print("회원가입 요청")
-    }
+    
 }
