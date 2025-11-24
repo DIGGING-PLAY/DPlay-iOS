fc1944b4836251c7ce162ee72119ef3de71c49ee
JoHyerin <gpfls468@gmail.com>
2025-11-24 17:01:18 +0900

    [Chore] #11 - Domain layer onboarding 폴더링 수정

--- DPlay-iOS/DPlay-iOS/Domain/Onboarding/UseCase/OnboardingUseCase.swift
+++ DPlay-iOS/DPlay-iOS/Domain/Onboarding/UseCase/OnboardingUseCase.swift
@@ -8,25 +8,9 @@
 import Foundation
 
 protocol OnboardingUseCase {
-    func validateNicknameDuplicate(nickname: Nickname) async throws -> Bool
-    func singUp(profile: Profile) async throws
+    
 }
 
 final class DefaultOnboardingUseCase: OnboardingUseCase {
     
-    private let onboardingRepository: OnboardingRepository
-    
-    init(onboardingRepository: OnboardingRepository) {
-        self.onboardingRepository = onboardingRepository
-    }
-    
-    //1. 닉네임 중복 검사
-    func validateNicknameDuplicate(nickname: Nickname) async throws -> Bool {
-        return try await onboardingRepository.validateNicknameDuplicate(nickname: nickname.value)
-    }
-    
-    //2. 회원가입
-    func singUp(profile: Profile) async throws {
-        try await onboardingRepository.singUp(nickname: profile.nickname.value, image: profile.profileImage)
-    }
 }
