7594f0653d16ba93925c27e60137170cd74c1480
JoHyerin <gpfls468@gmail.com>
2025-11-27 03:47:54 +0900

    [Feat] #14 - UILabel에 밑줄 적용하는 extension 구현

--- DPlay-iOS/DPlay-iOS/Global/Extension/UILabel+.swift
+++ DPlay-iOS/DPlay-iOS/Global/Extension/UILabel+.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UILabel {
+    
+    ///UILabel에 스타일 가이드 기반 폰트 스타일 적용
     func setTextStyle(_ fontStyle: DpalyFontStyle){
         let font = UIFont.dplayFont(fontStyle)
         let fontHeight = font.pointSize
@@ -29,4 +31,24 @@ extension UILabel {
             self.attributedText = attrString
         }
     }
+    
+    ///UILabel에 밑줄 추가
+    ///targetText 지정 시 특정 문자열에만 적용 가능
+    func setUnderline(targetText: String? = nil) {
+        let targetText = targetText ?? self.text ?? ""
+        let attributedString: NSMutableAttributedString
+
+        if let existing = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: existing)
+        } else {
+            attributedString = NSMutableAttributedString(string: targetText)
+        }
+        
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSMakeRange(0, attributedString.length))
+        
+        self.attributedText = attributedString
+    }
+
 }
