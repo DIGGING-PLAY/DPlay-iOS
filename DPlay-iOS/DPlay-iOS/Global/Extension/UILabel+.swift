74a050eb830f83f89481b19bd0565356f25e7ae3
JoHyerin <gpfls468@gmail.com>
2025-11-30 03:10:21 +0900

    [Feat] #17 - 특정 텍스트의 스타일 변경하는 highlightText 함수 추가

--- DPlay-iOS/DPlay-iOS/Global/Extension/UILabel+.swift
+++ DPlay-iOS/DPlay-iOS/Global/Extension/UILabel+.swift
@@ -29,4 +29,22 @@ extension UILabel {
             self.attributedText = attrString
         }
     }
+
+    /// 특정 text의 속성(폰트, 색상)을 바꿔주는 함수
+    func highlightText(targetText: String, font: UIFont? = nil, color: UIColor? = nil) {
+        guard let attributedText = self.attributedText else { return }
+
+        let attributedString = NSMutableAttributedString(attributedString: attributedText)
+        let range = (attributedText.string as NSString).range(of: targetText)
+        
+        if let font {
+            attributedString.addAttribute(.font, value: font, range: range)
+        }
+        
+        if let color {
+            attributedString.addAttribute(.foregroundColor, value: color, range: range)
+        }
+        
+        self.attributedText = attributedString
+    }
 }
