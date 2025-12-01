d51f86c0996548e1e732f04a22f097be70b8be5d
정정욱 <jeonguk29@naver.com>
2025-12-01 20:46:28 +0900

    [Feat] #20 - BASE_URL 읽는 코드 추가

---
+++ DPlay-iOS/DPlay-iOS/Network/Core/Config.swift
@@ -0,0 +1,32 @@
+//
+//  Config.swift
+//  DPlay-iOS
+//
+//  Created by 정정욱 on 12/1/25.
+//
+
+import Foundation
+
+enum Config {
+    enum Keys {
+        enum Plist {
+            static let BASE_URL = "BASE_URL"
+        }
+    }
+    
+    private static let infoDictionary: [String: Any] = {
+        guard let dict = Bundle.main.infoDictionary else {
+            fatalError("plist cannot found !!!")
+        }
+        return dict
+    }()
+}
+
+extension Config {
+    static let baseURL: String = {
+        guard let key = Config.infoDictionary[Keys.Plist.BASE_URL] as? String else {
+            fatalError("🍞⛔️BASE_URL is not set in plist for this configuration⛔️🍞")
+        }
+        return "http://" + key
+    }()
+}
