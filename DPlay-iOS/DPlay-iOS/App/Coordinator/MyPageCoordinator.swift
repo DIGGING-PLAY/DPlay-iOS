d5cbdbe3a5b8a5cb486a5ee032398be527886003
JoHyerin <gpfls468@gmail.com>
2025-12-08 15:05:17 +0900

    [Feat] #17 - MyPageCoordinator 구현

---
+++ DPlay-iOS/DPlay-iOS/App/Coordinator/MyPageCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  MyPageCoordinator.swift
+//  DPlay-iOS
+//
+//  Created by 조혜린 on 12/2/25.
+//
+
+import UIKit
+
+final class MyPageCoordinator: Coordinator {
+
+    var childCoordinators: [Coordinator] = []
+    let navigationController: UINavigationController
+    
+    private let myPageService = MockMyPageService()
+    private lazy var myPageRepository = DefaultMyPageRepository(service: myPageService)
+    private lazy var myPageUseCase = DefaultMyPageUseCase(repository: myPageRepository)
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vm = MyPageViewModel(useCase: myPageUseCase, coordinator: self)
+        let vc = MyPageViewController(viewModel: vm)
+        navigationController.isNavigationBarHidden = true
+        navigationController.setViewControllers([vc], animated: false)
+    }
+
+    func pop() {
+        navigationController.popViewController(animated: true)
+    }
+}
