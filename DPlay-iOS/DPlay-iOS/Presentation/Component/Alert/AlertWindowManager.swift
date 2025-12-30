63142aa81d5d0976ad2eec19ab38f2d1c1a276ab
JoHyerin <gpfls468@gmail.com>
2025-12-31 02:43:02 +0900

    [Chore] #22 - 코드리뷰 반영: 주석 추가

--- DPlay-iOS/DPlay-iOS/Presentation/Component/Alert/AlertWindowManager.swift
+++ DPlay-iOS/DPlay-iOS/Presentation/Component/Alert/AlertWindowManager.swift
@@ -24,21 +24,25 @@ extension AlertWindowManager {
         message: String? = nil,
         actions: [AlertAction],
     ) {
+        //중복으로 띄우지 않도록 체크
         guard alertWindow == nil else { return }
         
+        //현재 화면에 떠 있는 활성 Scene을 찾아서 할당
         guard let scene = UIApplication.shared.connectedScenes
             .compactMap({ $0 as? UIWindowScene })
             .first(where: { $0.activationState == .foregroundActive }) else { return }
         
+        //새로 띄울 window의 roorViewController 선언 및 배경 제거 -> 아래 화면 노출되도록
         let rootViewController = UIViewController()
         rootViewController.view.backgroundColor = .clear
         
+        //윈도우 생성 및 위에서 찾은 scene과 매핑
         let window = UIWindow(windowScene: scene)
         window.rootViewController = rootViewController
-        window.windowLevel = .alert + 1
+        window.windowLevel = .alert + 1 //windowLevel을 최상단으로 보장
         window.backgroundColor = .clear
         window.isHidden = false
-        window.makeKeyAndVisible()
+        window.makeKeyAndVisible() //키 윈도우 지정 및 화면에 표시
         
         alertWindow = window
         
