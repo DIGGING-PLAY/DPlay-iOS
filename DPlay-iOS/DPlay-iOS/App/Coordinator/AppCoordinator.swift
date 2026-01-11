2d3c28cff00fbbed5aa1d0fefd189b1f68e624de
JoHyerin <gpfls468@gmail.com>
2026-01-12 02:17:58 +0900

    [Fix] #32 - AppCoordinator 앱 진입점 변경 및 root 전환 로직 추가

--- DPlay-iOS/DPlay-iOS/App/Coordinator/AppCoordinator.swift
+++ DPlay-iOS/DPlay-iOS/App/Coordinator/AppCoordinator.swift
@@ -18,12 +18,51 @@ final class AppCoordinator: Coordinator {
     }
 
     func start() {
+        let authFlowCoordinator = AuthFlowCoordinator()
+        childCoordinators.append(authFlowCoordinator)
+        
+        // 메인 탭바로 root 교체하는 클로저
+        authFlowCoordinator.onFinishAuthFlow = { [weak self, weak authFlowCoordinator] in
+            guard let self else { return }
+            
+            if let authFlowCoordinator {
+                self.removeChild(authFlowCoordinator)
+            }
+            
+            self.showMainTabBar()
+        }
+        
+        authFlowCoordinator.start()
+        setRootViewController(authFlowCoordinator.rootViewController, animated: false)
+    }
+}
+
+private extension AppCoordinator {
+    func showMainTabBar() {
         let tabBarCoordinator = TabBarCoordinator()
         childCoordinators.append(tabBarCoordinator)
-
         tabBarCoordinator.start()
-
-        window.rootViewController = tabBarCoordinator.rootViewController
+        
+        setRootViewController(tabBarCoordinator.rootViewController, animated: true)
+    }
+    
+    ///현재 window의 rootViewController를 변경하는 함수
+    func setRootViewController(_ vc: UIViewController, animated: Bool) {
+        if animated {
+            UIView.transition(
+                with: window,
+                duration: 0.25,
+                options: .transitionCrossDissolve,
+                animations: { self.window.rootViewController = vc }
+            )
+        } else {
+            window.rootViewController = vc
+        }
         window.makeKeyAndVisible()
+    }
+    
+    
+    func removeChild(_ coordinator: Coordinator) {
+        childCoordinators.removeAll { $0 === coordinator }
     }
 }
