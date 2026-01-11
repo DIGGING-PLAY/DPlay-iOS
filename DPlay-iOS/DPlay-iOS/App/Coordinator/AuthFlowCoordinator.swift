7ec2438d3b57cbfe877a5cb165acd6469c9ead55
JoHyerin <gpfls468@gmail.com>
2026-01-12 02:16:13 +0900

    [Feat] #32 - AuthFlowCoordinator 구현

---
+++ DPlay-iOS/DPlay-iOS/App/Coordinator/AuthFlowCoordinator.swift
@@ -0,0 +1,47 @@
+//
+//  AuthFlowCoordinator.swift
+//  DPlay-iOS
+//
+//  Created by 조혜린 on 1/9/26.
+//
+
+import UIKit
+
+final class AuthFlowCoordinator: Coordinator {
+
+    var childCoordinators: [Coordinator] = []
+    var rootViewController = UIViewController()
+    
+    var onFinishAuthFlow: (() -> Void)?
+    
+    private let authService = AuthServiceImpl()
+    private lazy var authRepository = DefaultAuthRepository(service: authService)
+    private lazy var authUseCase = DefaultAuthUseCase(repository: authRepository)
+
+    func start() {
+        let vc = SplashViewController()
+        rootViewController = vc
+        
+        Task { @MainActor in
+            try? await Task.sleep(nanoseconds: 1_000_000_000)
+            startLoginFlow()
+        }
+    }
+}
+
+extension AuthFlowCoordinator {
+    private func startLoginFlow() {
+        let loginViewModel = LoginViewModel(useCase: authUseCase, coordinator: self)
+        let loginViewController = LoginViewController(viewModel: loginViewModel)
+
+        loginViewController.modalPresentationStyle = .fullScreen
+        loginViewController.modalTransitionStyle = .crossDissolve
+        
+        rootViewController.present(loginViewController, animated: true)
+    }
+    
+    func goToMainTabBar() {
+        //window의 roorViewController를 변경하기 위해 AppCoordinator에게 요청
+        onFinishAuthFlow?()
+    }
+}
