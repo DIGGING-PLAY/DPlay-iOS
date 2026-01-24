420ea68dd33aca1931a391e3ba503095de0e49bc
정정욱 <jeonguk29@naver.com>
2026-01-25 01:35:11 +0900

    [Feat] #38 - 핑크 좋아요 fill 이미지 추가

--- DPlay-iOS/DPlay-iOS/Global/Extension/Iamge&IconLiterals.swift
+++ DPlay-iOS/DPlay-iOS/Global/Extension/Iamge&IconLiterals.swift
@@ -16,6 +16,7 @@ enum IconLiterals {
     static var ic_heart_w: UIImage { .load(name: "ic_heart_w_24") }
     static var ic_heart_w_fill: UIImage { .load(name: "ic_heart_w_fill_24") }
     static var ic_heart_p: UIImage { .load(name: "ic_heart_p_24") }
+    static var ic_heart_p_fill: UIImage { .load(name: "ic_heart_p_fill_24") }
     static var ic_editor: UIImage { .load(name: "ic_editor") }
     static var ic_editor_20: UIImage { .load(name: "ic_editor_20") }
     static var ic_new: UIImage { .load(name: "ic_new_20") }
