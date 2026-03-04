1a0b564cace9d2fbcf915f2068de0721f2779fec
정정욱 <jeonguk29@naver.com>
2026-03-04 20:23:11 +0900

    [Chore] #62 - 공용 Entity 분리

---
+++ DPlay-iOS/DPlay-iOS/Domain/Common/Entity/Badge.swift
@@ -0,0 +1,15 @@
+//
+//  Badge.swift
+//  DPlay-iOS
+//
+//  Created by 정정욱 on 11/7/25.
+//
+
+import Foundation
+
+enum Badge: String {
+    case editor = "EDITOR"
+    case best   = "BEST"
+    case new    = "NEW"
+    case nomal
+}
