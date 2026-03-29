a998d4f92a0b9c1b7a25b560ba3bb3461f79564f
정정욱 <jeonguk29@naver.com>
2026-03-29 23:27:06 +0900

    [Chore] - Xcode Cloud용 Config.xcconfig 생성 스크립트 경로 수정

--- DPlay-iOS/ci_scripts/ci_post_clone.sh
+++ DPlay-iOS/ci_scripts/ci_post_clone.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cat <<INNER > "$CI_PRIMARY_REPOSITORY_PATH/DPlay-iOS/Config.xcconfig"
+cat <<INNER > "$CI_PRIMARY_REPOSITORY_PATH/DPlay-iOS/DPlay-iOS/Config.xcconfig"
 BASE_URL = $BASE_URL
 INNER
